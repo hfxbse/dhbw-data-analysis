1e2298aa311daed93b1410146951358d65cb9373
Fabian Haas <29468630+hfxbse@users.noreply.github.com>
2024-12-05 00:58:26 +0100

    Link Nix R packages to the user library path
    Required for JetBrains R plugin to pick the libraries up

--- flake.nix
+++ flake.nix
@@ -34,9 +34,23 @@
     # Allow traditional interactive package installation
     # Useful for JetBrains IDEs which do not pickup libraries installed through Nix
     devShells.rInteractive = self.devShells.${system}.rMarkdown.overrideAttrs (previous: {
-      packages = [
-        pkgs.gnumake
+      packages = with pkgs; [
+        curl
+        gnumake
+        libgcc
+        libxml2
       ] ++ (previous.packages or []);
+
+
+      # Link system R packages to user library directory
+      shellHook = ''
+        eval "$(head -n -1 $(which R))"
+        USER_LIBS_PATH=$(echo ".libPaths()" | R --slave | head -n 1 | cut -d ' ' -f 3 | tr -d '"')
+
+        for SYSTEM_LIB_PATH in ''${R_LIBS_SITE//:/$'\n'} ; do
+            ln -sf "$SYSTEM_LIB_PATH"/* $USER_LIBS_PATH
+        done
+      '';
     });
 
     devShells.default = self.devShells.${system}.rMarkdown;
